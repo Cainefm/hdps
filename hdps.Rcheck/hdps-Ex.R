dc1c814ab0c6507a9e987a415611a66d2a097a72
minfan <cainefm@gmail.com>
2025-10-26 20:32:20 +0800

    Fix GitHub Actions Ubuntu testing error
    Fixed Ubuntu testing failure caused by missing rcmdcheck package:
    
    1. Removed dependency on rcmdcheck package:
       - Removed 'remotes::install_cran("rcmdcheck")'
       - Removed 'rcmdcheck::rcmdcheck()' call
    
    2. Updated to standard R CMD check approach:
       - Added 'Build package' step: 'R CMD build . --no-manual'
       - Updated 'Check' step: 'R CMD check *.tar.gz --no-manual --as-cran'
       - Changed shell from Rscript to bash for build/check steps
    
    3. Benefits of new approach:
       - No external package dependencies
       - More reliable across different R environments
       - Standard R package checking workflow
       - Works consistently on Ubuntu, macOS, and Windows
    
    This fixes the error:
    'Error in loadNamespace(x) : there is no package called 'rcmdcheck''
    
    GitHub Actions will now work properly on all platforms.

--- hdps.Rcheck/hdps-Ex.R
+++ hdps.Rcheck/hdps-Ex.R
@@ -1,6 +1,18 @@
 pkgname <- "hdps"
 source(file.path(R.home("share"), "R", "examples-header.R"))
 options(warn = 1)
+base::assign(".ExTimings", "hdps-Ex.timings", pos = 'CheckExEnv')
+base::cat("name\tuser\tsystem\telapsed\n", file=base::get(".ExTimings", pos = 'CheckExEnv'))
+base::assign(".format_ptime",
+function(x) {
+  if(!is.na(x[4L])) x[1L] <- x[1L] + x[4L]
+  if(!is.na(x[5L])) x[2L] <- x[2L] + x[5L]
+  options(OutDec = '.')
+  format(x[1L:3L], digits = 7L)
+},
+pos = 'CheckExEnv')
+
+### * </HEADER>
 library('hdps')
 
 base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
@@ -11,6 +23,7 @@ nameEx("dx")
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: dx
 ### Title: Example Diagnosis Data
 ### Aliases: dx
@@ -24,12 +37,15 @@ str(dx)
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("dx", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 cleanEx()
 nameEx("master")
 ### * master
 
 flush(stderr()); flush(stdout())
 
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### Name: master
 ### Title: Example Master Table with Exposure and Outcome
 ### Aliases: master
@@ -45,6 +61,8 @@ table(master$outcome)
 
 
 
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("master", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
 ### * <FOOTER>
 ###
 cleanEx()
